7d93d6f18d04a888d30944a272f0fee6a2c4d157
buhaiqing <haiqing.bu@163.com>
2018-01-29 18:44:14 +0800

    Update reader.go

--- reader.go
+++ reader.go
@@ -68,33 +68,3 @@ func ReadLine(r *bufio.Reader) ([]byte, error) {
 	return line, err
 }
 
-func ReadLines(path string) (lines [] string, err error) {
-	var (
-		_file   *os.File
-		part   [] byte
-		prefix bool
-	)
-
-	if _file, err = os.Open(path); err != nil {
-		return
-	}
-
-	reader := bufio.NewReader(_file)
-	buffer := bytes.NewBuffer(make([]byte, 1024))
-
-	for {
-		if part, prefix, err = reader.ReadLine(); err != nil {
-			break
-		}
-		buffer.Write(part)
-		if !prefix {
-			lines = append(lines, buffer.String())
-			buffer.Reset()
-		}
-	}
-	if err == io.EOF {
-		err = nil
-	}
-	return
-}
-
