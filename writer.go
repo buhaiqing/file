063ca258dc5276cbed6170365cad41fc3a8980c0
buhaiqing <haiqing.bu@163.com>
2018-01-29 18:29:07 +0800

    Update writer.go

--- writer.go
+++ writer.go
@@ -18,3 +18,23 @@ func WriteBytes(filePath string, b []byte) (int, error) {
 func WriteString(filePath string, s string) (int, error) {
 	return WriteBytes(filePath, []byte(s))
 }
+
+func writeLines(lines [] string,path string)(err error){
+	var file *os.File
+
+	if file,err = os.Create(path); err != nil{
+		return
+	}
+
+	defer file.Close()
+
+	for _,elem := range lines {
+		_,err := file.WriteString(strings.TrimSpace(elem)+"\n")
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+	}
+	return
+}
+
